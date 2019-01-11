ebedfb6439726757ce06d5bc5f72a06c044a3775
tanjie <248824711@qq.com>
2019-01-11 09:59:37 +0800

    comment mainFuncation

--- USER/main.c
+++ USER/main.c
@@ -21,12 +21,12 @@
 	LED_Init();		  	//初始化与LED连接的硬件接口
 	while(1)
 	{
-		LED0=0;
-		LED1=1;
-		delay_ms(300);	 //延时300ms
-		LED0=1;
-		LED1=0;
-		delay_ms(300);	//延时300ms
+//		LED0=0;
+//		LED1=1;
+//		delay_ms(300);	 //延时300ms
+//		LED0=1;
+//		LED1=0;
+//		delay_ms(300);	//延时300ms
 	}
  }
 
